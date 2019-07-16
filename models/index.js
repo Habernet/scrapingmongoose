729a8f80c9d35b2211e3916c4ee194c050b9577c
habernet <phaberern@gmail.com>
2019-07-15 21:06:34 -0400

    Article model and comment model written

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// Export an object that contains all of ourmodels. The models are uppercase by convention.
+module.exports = {
+  Article: require("./article"),
+  Comment: require("./comment")
+};
